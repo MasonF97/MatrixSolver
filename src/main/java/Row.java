e8b2b279913ea646fb7b42a7f5e701a540dac59c
MasonF97 <mlf4819@rit.edu>
2024-04-16 13:18:15 -0400

    ROw row OPs

--- src/main/java/Row.java
+++ src/main/java/Row.java
@@ -1,5 +1,27 @@
 package src.main.java;
 
+import java.util.ArrayList;
+
 public class Row {
+    private ArrayList<Integer> row;
     
+    public Row(ArrayList<Integer> row){
+        this.row = row;
+    }
+
+    public void addRow(Row other, int scalar){
+        for(int i = 0;i<row.size();i++){
+            row.set(i, row.get(i)+(scalar*other.getAtIndex(i)));
+        }
+    }
+
+
+    public void multiplyRow(int scalar){
+        for(int i = 0;i<row.size();i++){
+            row.set(i, row.get(i)*scalar);
+        }
+    }
+
+    public int getAtIndex(int i){ return row.get(i);}
+    public ArrayList<Integer> getRow(){return this.row;}
 }
