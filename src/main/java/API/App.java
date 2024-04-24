558bfe8ff933209d45bd69b4b7a33d5f93cebfbc
MasonF97 <mlf4819@rit.edu>
2024-04-24 15:17:24 -0400

    start of CLI

--- src/main/java/API/App.java
+++ src/main/java/API/App.java
@@ -1,9 +1,28 @@
 package src.main.java.API;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception{
-        float[][] floats = {{1,2,3,4}, {0,1,2,3}, {0,0,2,4}, {0,0,0,1}};
-        float[] rhs = {1,2,3,4};
-        Matrix matrix = new Matrix(floats, rhs);
-        System.out.println(matrix.toRREF());
+        Scanner scan = new Scanner(System.in);
+        boolean run = true;
+        while(run){
+            System.out.println("Enter 1 to solve a system of equations, 2 to reduce a matrix, 3 to perform matrix multiplication, or 4 to quit");
+            switch(scan.nextLine()){
+                case "1": 
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    run = false;
+                    break;
+                default: 
+                    System.out.println("Invalid input");
+                    continue;
+            }
+        }
+        scan.close();
     }
 }
